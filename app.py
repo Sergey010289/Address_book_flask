34b17764a1d33fcecb4ff6f9601d8aa64f42b659
Sergey Muravko <sergeymuravko@gmail.com>
2014-09-26 17:28:16 +0300

    # clear trash

--- app.py
+++ app.py
@@ -2,62 +2,27 @@
 
 from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from flask_jwt import JWT, jwt_required
-
+from models import Users, Contacts
 from flask import request, Response
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import func
 import json
-#from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task2.db'
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+
 engine = create_engine('sqlite:///task2.db')
 Session = sessionmaker(bind=engine)
+
+app = Flask(__name__)
+
 db = SQLAlchemy(app)
-from models import Users, Contacts
+
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['JWT_EXPIRATION_DELTA'] = 3600   # time life token
 
 jwt = JWT(app)
-
-
-# class Users(db.Model):
-#     __tablename__ = 'users'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255), nullable=False)
-#     surname = db.Column(db.String(255), nullable=False)
-#     salt = db.Column(db.String(255), nullable=True)
-#     enc_passwd = db.Column(db.String(255), nullable=False)
-#     addresses = db.relationship('Contacts', backref='user')
-#
-#     def __init__(self, name, surname, salt, enc_passwd):
-#         self.name = name
-#         self.surname = surname
-#         self.salt = salt
-#         self.enc_passwd = enc_passwd
-#
-#     def __repr__(self):
-#         return "{0}: {1}".format(self.name, self.surname,)
-#
-#
-# class Contacts(db.Model):
-#     __tablename__ = 'contacts'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     contact = db.Column(db.String(255), nullable=False)
-#     contact_type = db.Column(db.String(255), nullable=False)    # enum(tel, email, skype)
-#     users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#
-#     def __init__(self, contact, contact_type, users_id):
-#         self.contact = contact
-#         self.contact_type = contact_type
-#         self.users_id = users_id
-#
-#     def __repr__(self):
-#         return "{0}: {1}".format(self.contact, self.contact_type,)
 
 
 class User(object):
